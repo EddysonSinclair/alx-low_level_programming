893f0f20893a56d227fb5a4ff2b0817853e19246
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-08-16 05:07:49 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,22 @@
 * is written by mbah
 * Return: 0
 */
+
 int main(void)
 {
 _putchar('_');
 _putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
 _putchar('\n');
+_putchar('u');
+_putchar('\n');
+_putchar('t');
+_putchar('\n');
+_putchar('c');
+_putchar('\n');
+_putchar('h');
+_putchar('\n');
+_putchar('a');
+_putchar('\n');
+_putchar('r');
 return (0);
 }
