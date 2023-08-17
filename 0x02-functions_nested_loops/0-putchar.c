074a2cd45e2590a10c78129e7862a2f750a4a887
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-08-17 06:24:02 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - print _putchar
-* is written by mbah
+* is written by Eddyson
 * Return: 0
 */
 
@@ -9,17 +9,12 @@ int main(void)
 {
 _putchar('_');
 _putchar('p');
-_putchar('\n');
 _putchar('u');
-_putchar('\n');
 _putchar('t');
-_putchar('\n');
 _putchar('c');
-_putchar('\n');
 _putchar('h');
-_putchar('\n');
 _putchar('a');
-_putchar('\n');
 _putchar('r');
+_putchar('\n');
 return (0);
 }
