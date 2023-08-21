0ea3400108deaa86455fdd74ab4cbf86c118fb4b
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-08-21 08:47:07 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
 *
 * Return:void
 */
-void print_alphabet(void);
+void print_alphabet(void);{
 int main(void){
 	char ch;
 	for(ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	printf("%c \n", ch);
+	return(void);
 }
 }
