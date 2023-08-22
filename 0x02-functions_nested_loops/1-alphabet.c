c6ce9085bbb31748607b4c97731224182b2e2c1b
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-08-22 07:06:45 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,17 @@
 *
 * Return:void
 */
+void _putchar(char c) {
+    write(1, &c, 1);
+}
 
-void print_alphabet(void)
-{
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
+void print_alphabet(void) {
+    char letter = 'a';
+
+    while (letter <= 'z') {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
 }
