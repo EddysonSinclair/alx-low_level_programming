d525dd35e28fececf0263857c12ecb24a582fa27
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-08-28 00:13:48 +0100

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,35 @@
-#include "main.h"
-/**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
- */
-void puts2(char *str)
-{
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-	while (*y != '\0')
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		y++;
-		longi++;
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
-	}
-	_putchar('\n');
-}
+
+	return (0);
+}}
