c8eece623a1c60332c7761511d842153c4d63663
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-10-06 09:49:59 +0100

    static

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+# Compile each .c file into a .o file
+for file in *.c; do
+    gcc -c "$file" -o "${file%.c}.o"
+done
+
+# Create the static library liball.a from all .o files
+ar rc liball.a *.o
+
+# Clean up the temporary .o files
+rm -f *.o
+
