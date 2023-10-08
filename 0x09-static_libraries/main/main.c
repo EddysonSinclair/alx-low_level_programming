6e36aea7d47e547411006e8cc92fceeda97b3774
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-10-08 06:12:03 +0000

    static

--- 0x09-static_libraries/main/main.c
+++ 0x09-static_libraries/main/main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-void main(void)
+int main(void)/*This is the main function of the code*/
 {_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
 	_puts("- Kevin Mitnick\n");
 return (0);
