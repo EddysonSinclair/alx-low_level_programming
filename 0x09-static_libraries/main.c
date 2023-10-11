52a1bfeec3e2b7a85d6d16c917c512f7ced20cd3
Eddy_sin <101008331+EddysonSinclair@users.noreply.github.com>
2023-10-11 13:12:55 +0100

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/main.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-int main(void)/*This is the main function of the code*/
-{_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
-	_puts("- Kevin Mitnick\n");
-return (0);
-}
