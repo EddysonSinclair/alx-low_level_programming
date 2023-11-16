206ab9ea5c434f25b7d375d7c5f0d6702e5714a4
EddysonSinclair <Eddysonsinclair@gmail.com>
2023-11-16 11:47:39 +0000

    recursions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,14 @@
 #include "main.h"
+/**
+ * _puts_recursion - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * Return: -
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 {
 	_putchar('\n');
-
-	return;
 }
 else
 _putchar(*s);
