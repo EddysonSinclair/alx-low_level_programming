c257ce84ad7839d3edc999d5234a68f352349d44
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-19 22:44:43 +0000

    recursions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,11 +6,11 @@ int is_prime_number2(int n,int stop, int i)
 {
 if (i >= stop)
 {
-return 1;
+return (1);
 }
 else if (n % i == 0)
 {
-return 0;
+return (0);
 }
 else
 { 
@@ -23,11 +23,11 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 {
-return 0;
+return (0);
 }
 else if(n == 2)
 {
-return 1;
+return (1);
 }
 else
 {
