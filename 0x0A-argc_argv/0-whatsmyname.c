5bbe4f2469a4bd04e12da7cd80a874ce6d50595f
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-22 15:00:17 +0000

    argv/argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: -
+ * Return: -
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s", argv[0]);
+	}
+	return (0);
+}
