81769207ec502f8539f0d56d9c1e8aad06528a0a
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-23 12:31:02 +0000

    argc/argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	else
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+
+		printf("%d\n", a * b);
+	}
+return (0);
+}
