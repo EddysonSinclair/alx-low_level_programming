9a3122fd0a4b115625d2a2efba7b86459e01a43d
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-26 14:51:15 +0000

    malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,15 +12,13 @@ char *_strdup(char *str)
 	int i;
 	char *s;
 
-	length = strlen(str);
-
 	if (str == NULL)
 		return (NULL);
+	 length = strlen(str);
 	s = (char *) malloc(sizeof(char) * (length + 1));
 
 	for (i = 0; i < length; i++)
 		s[i] = str[i];
 	return (s);
-	free(s);
 
 }
