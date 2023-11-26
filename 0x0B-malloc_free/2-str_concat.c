a62d13463667acf4af6cdf819c2b4bceb63468b2
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-26 22:17:08 +0000

    malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -36,6 +36,6 @@ char *str_concat(char *s1, char *s2)
 		ptr[i] = s1[i];
 	for (i = 0; i < length2; i++)
 		ptr[length1 + i] = s2[i];
-	ptr[total_length] = '\0';
+	ptr[total_length - 1] = '\0';
 	return (ptr);
 }
