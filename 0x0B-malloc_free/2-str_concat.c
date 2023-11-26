87517c83de708183c2a90d569ab9b8dd2be795c0
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-26 20:28:09 +0000

    malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	else if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
 		return (NULL);
 	 length1 = strlen(s1);
 	 length2 = strlen(s2);
