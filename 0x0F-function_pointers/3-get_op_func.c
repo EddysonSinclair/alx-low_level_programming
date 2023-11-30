5b4a156ff24f44d3a1c6011b19bab3c5f21bdf36
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-30 23:06:43 +0000

    ptr2func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
  * get_op_func - selects the correct function to perform
  * the operation asked by the user.
@@ -28,5 +27,4 @@ int (*get_op_func(char *s))(int, int)
 	}
 
 	return (ops[i / 2].f);
-
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+
 /**
  * op_add - adds two numbers.
  * @a: first number.
@@ -12,33 +13,36 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - function that subtracts two numbers
- * @a: first integer
- * @b: second integer
- * Return: differencce of two numbers
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - multiplies two numbers
- * @a: first integer
- * @b: second integer
- * Return: product of two numbers
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-/**
- * op_div - divides two numbers
- * @a: first integer
- * @b: second integer
- * Return: division of two numbers
- */
 
+/**
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -48,13 +52,14 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
-/**
- * op_mod - adds two numbers
- * @a: first integer
- * @b: second integer
- * Return: remainder of division of two numbers
- */
 
+/**
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
