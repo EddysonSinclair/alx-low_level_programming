c20c754ae1c8dce4b84f1e28edb85529cc0c57fa
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-11-30 15:01:15 +0000

    ptr2fnc

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef __function_pointers__
 #define __function_pointers__
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
