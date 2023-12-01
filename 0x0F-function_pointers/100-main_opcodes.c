883e1f1a22a63b7d637bf501a09526600aa5e31c
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-12-01 06:59:03 +0000

    ptr2func

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	unsigned char *opcodes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	opcodes = (unsigned char *)&main;
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", opcodes[i]);
+	}
+	printf("\n");
+	return (0);
+
+
+}
