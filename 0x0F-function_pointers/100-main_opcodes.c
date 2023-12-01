699b4cd6bcb6430e0aa52ec38ae183cad63cf4f1
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-12-01 07:05:49 +0000

    ptr2func

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,14 +15,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit (1);
+		exit(1);
 	}
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
 	{
 		printf("Error\n");
-		exit (2);
+		exit(2);
 	}
 	opcodes = (unsigned char *)&main;
 	for (i = 0; i < bytes; i++)
