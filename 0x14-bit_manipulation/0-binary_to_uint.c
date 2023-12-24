72b2d525572652e8daf069a2d780c4ddd682aa45
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-12-24 22:02:52 +0000

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,28 +9,25 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned long int i;
 	unsigned long int c = 0;
-	unsigned long int length = strlen(b);
-	unsigned long int *array = malloc(length * sizeof(unsigned long int));
+	unsigned long int length;
 
 	if (b == NULL)
 		return (0);
+
+	length = strlen(b);
 
 	for (i = 0; i < length; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
-			free(array);
 			return (0);
 		}
-
-		array[i] = b[i] - '0';
 	}
 
 	for (i = 0; i < length; i++)
 	{
 		c = c * 2 + (b[i] - '0');
 	}
-	free(array);
 	return ((unsigned int)c);
 
 
