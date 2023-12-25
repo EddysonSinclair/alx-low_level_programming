77fc61cef0363c12d94b76aa3aa3d1387417ef02
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-12-25 21:12:57 +0000

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,11 +8,13 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i;
-	unsigned int c = 0;
-	unsigned int length = 0;
+	unsigned int c;
+	unsigned int length;
 
 	if (b == NULL)
 		return (0);
+	c = 0;
+	length = 0;
 
 	for (; b[length] != '\0'; length++)
 		;
