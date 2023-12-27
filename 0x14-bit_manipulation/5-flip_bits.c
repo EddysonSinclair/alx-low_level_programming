cd4ce667ed6d12981ed4d02e1b2f8c94e1ac9e67
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-12-27 10:30:06 +0000

    bit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,25 +12,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int result;
 
 	result = n ^ m;
-	count = count_bit(result);
-	return (count);
-}
-
-/**
- * count_bit - returns numbers of flipped bits required.
- * @number: integer number.
- * Return: integer.
- */
-
-unsigned int count_bit(unsigned long int number)
-{
-	unsigned int count = 0;
-
-	while (number != 0)
+	for (count = 0; result > 0;)
 	{
-		if ((number & 1) == 1)
+		if ((result & 1) == 1)
 			count++;
-		number = number >> 1;
+		result = result >> 1;
 	}
 	return (count);
 }
