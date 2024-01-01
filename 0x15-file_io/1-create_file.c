52f6053084198f8eb7b017bd2e99fff1be1c3756
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-01-01 04:42:02 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
-	textlength = strlen(text_content);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
+
+	for (textlength = 0; text_content[textlength]; textlength++)
+		;
 
 	w = write(fd, text_content, textlength);
 
