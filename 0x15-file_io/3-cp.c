1607c44e0c02b6f916b4fb56e9a4b0d1078e391e
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-01-20 00:25:18 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -64,12 +64,17 @@ char *create_buff(char *file)
 
 
 /**
- * main - main function.
- * @argc: arg count.
- * @argv: arg vector.
- * Return: integer.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to;
