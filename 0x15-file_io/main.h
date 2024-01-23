f218db322cd409702db318473e898805d48f87df
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-01-23 13:26:12 +0000

    taskk 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error98_checker(int file1,char *buffer, char *argv);
-void error99_checker(int file1, char *buffer, char *argv);
-void error_cases(int file);
 #endif
