05d0a002bd8b784df204967c9fb84ff62f4aed9c
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-02-03 10:47:56 +0000

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,23 +9,22 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
 	int count;
 
 	count = 0;
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 		}
 
 		else
 		{
-			printf("[%d] %s \n", current->len, current->str);
+			printf("[%d] %s \n", h->len, h->str);
 		}
 		count++;
-		current = current->next;
+		h = h->next;
 	}
 	return (count);
 }
