9849a2c1a640fbebb7dceb46343670f32a94ebac
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-02-07 00:44:58 +0000

    singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - frees the memory in a list
+ * @head: it is the head.
+ * Return: integer.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;	
+	}
+
+}
