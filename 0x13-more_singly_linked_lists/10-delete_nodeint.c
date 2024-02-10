93d0b31a9fba399336355d86de4d3a313a68b867
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-02-10 12:54:01 +0000

    more singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 	tmp = *head;
+	prev = NULL;
 
 	if (index == 0)
 	{
@@ -28,6 +29,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			free(tmp);
 			return (1);
 		}
+		if (index > i)
+			return (-1);
 		prev = tmp;
 		tmp = tmp->next;
 	}
