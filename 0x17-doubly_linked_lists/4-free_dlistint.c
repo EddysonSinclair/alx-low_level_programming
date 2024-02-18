9d8cd150a5a6d7e4434e54b084fc00054b465df2
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-02-18 09:57:27 +0000

    doubly linked lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_dlistint -  frees a dlistint_t list.
+ * @head: pointer to the head of the func.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+
+		if (tmp)
+			temp->prev = NULL;
+
+	}
+}
