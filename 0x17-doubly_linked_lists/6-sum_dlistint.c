1005578bd0d23524e3a403dd85777c892f81e2ec
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-02-19 11:43:03 +0000

    doubly linked lists

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the nth node of a dlistint_t linked list.
+ * @head: pointer to the head of the list
+ * Return: integer
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+	int data;
+	sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		data = head->n;
+		sum += data;
+		head = head->next;
+	}
+	return (sum);
+}
